8dfededd6e799d92dc4e3ae0a3563f6afe61006d
fanhengbin <8334632@qq.com>
2019-03-22 14:49:51 +0800

    商品详情页

---
+++ src/assets/javascript/details.js
@@ -0,0 +1,28 @@
+$(function(){
+// 按钮选中效果
+$(".property-sibling-item dd a").click(
+    function(){
+        $(this).css({"border-color":" #000","color":"#000"}).siblings("a").css({"border-color":" #BFBFBF","color":"#595959"});
+       console.log($(this));
+    }
+)
+// 商品数量
+$("#minus").click(
+    function(){
+        var inputVal=$(this).next()
+        var val=parseInt(inputVal.val());
+        if(val==0){return;}
+        var num=val-1;
+        inputVal.val(num);
+    }
+)
+$("#add").click(
+    function(){
+        var inputVal=$(this).prev()
+        var val=parseInt(inputVal.val());
+        // if(val==0){return;}
+        var num=val+1;
+        inputVal.val(num);
+    }
+)
+});
