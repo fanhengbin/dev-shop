07d4e3ac48e849a6a03066ceadbce906066e0d2d
ASYCN\Chuanya.Xu <122951508@qq.com>
2019-03-25 18:17:13 +0800

    debug by xu

--- src/assets/javascript/getOrderInfo.js
+++ src/assets/javascript/getOrderInfo.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     $('.order-address-check.checked').mouseover(function(){
-        $('.order-address-check-ctrl').show();
+        var index3 = $('.order-address-check.checked').index(this);
+        $('.order-address-check-ctrl').eq(index3).show();
     });
     $('.order-address-check.checked').mouseout(function(){
-        $('.order-address-check-ctrl').hide();
+        var index3 = $('.order-address-check.checked').index(this);
+        $('.order-address-check-ctrl').eq(index3).hide();
     });
     $('.order-address-check.add').mouseover(function(){
         $('.iconfont.icon-zengjia').css("color","rgb(213, 28, 41)");
@@ -14,14 +16,18 @@ $(document).ready(function(){
         $('.order-address-add-text').css("color","#aaa");
     });
     $('.delete').click(function(){
-        $('.order-address-check.checked').remove();
+        var index1 = $('.delete').index(this);
+        $('.order-address-check.checked').eq(index1).remove();
     });
     var div = document.getElementById('background');  
     $('.order-address-check.add').click(function(){
-        div.style.display = "block";
+        // div.style.display = "block";
+        $('#background').css("display","block")
     });
     $('.edit').click(function(){
-        div.style.display = "block";
+        // div.style.display = "block";
+        // var index2 = $('.edit').index(this);
+        $('#background').css("display","block")
     });
     $('#close-button').click(function(){
         div.style.display = "none";
