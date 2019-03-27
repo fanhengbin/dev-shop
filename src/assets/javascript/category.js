fe29748da3a5dbd8e5ae177abea313ccd2824466
ASYCN\Martha.Ren <735568868@qq.com>
2019-03-27 11:09:23 +0800

    category部分

--- src/assets/javascript/category.js
+++ src/assets/javascript/category.js
@@ -1,12 +1,20 @@
 $(function () {
+    //筛选列表 active特效 ----开始
     $(".filter-order a").click(function () {
         $(this).siblings('a').removeClass('active'); // 删除其兄弟元素的样式
         $(this).addClass('active'); // 为点击元素添加类名
     });
+    $(".list-inline a").click(function () {
+        $(this).siblings('a').removeClass('active'); // 删除其兄弟元素的样式
+        $(this).addClass('active'); // 为点击元素添加类名
+    });
+    //筛选列表 active特效 ----结束
+    //价格图标切换
     $('.sort-price').on('click', function (ev) {
         $('.sort-price').toggleClass('iconfont icon-xiangxiajiantou').toggleClass('iconfont icon-xiangshangjiantou');
     });
 });
+//检测返回首部
 window.onscroll = function () {
     var t = document.documentElement.scrollTop;
     if (t >= 600) {
