4d60e55a520bc0559952dc6eae2869f06f07eb85
ASYCN\Martha.Ren <735568868@qq.com>
2019-03-28 14:07:16 +0800

    更新mini-cart

--- src/assets/javascript/minicart.js
+++ src/assets/javascript/minicart.js
@@ -1,8 +1,21 @@
 $(function () {
     function remove(obj) {
         var list = obj.closest('div');
+        list.remove();
+        Emptycart();
     }
     $('.cart-item span').click(function () {
         remove($(this));
     });
+    function Emptycart() {
+        var unload = $('.unloaded-nav .cart-item').length;
+        var load = $('.loaded-nav .cart-item').length;
+        if ((unload <= 0) || (load <= 0)) {
+            $(".empty-cart").show();
+            $('.exist-cart').hide();
+        }
+        else {
+            return false;
+        }
+    }
 })
