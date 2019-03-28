664eb3ae02b4508109b05699db6efcb758ba2c9d
ASYCN\Martha.Ren <735568868@qq.com>
2019-03-28 15:35:33 +0800

    minicart优化

--- src/assets/javascript/minicart.js
+++ src/assets/javascript/minicart.js
@@ -1,21 +1,21 @@
 $(function () {
+    function Emptycart() {
+        var unload = $('.unloaded-nav .cart-item').length;
+        var load = $('.loaded-nav .cart-item').length;
+        if(unload <= 0){
+            $('.unloaded-nav .empty-cart').show();
+            $('.unloaded-nav .exist-cart').hide();
+        }else if (load <=0){
+            $('.loaded-nav .empty-cart').show();
+            $('.loaded-nav .exist-cart').hide();
+        };
+    }
     function remove(obj) {
         var list = obj.closest('div');
         list.remove();
         Emptycart();
     }
-    $('.cart-item span').click(function () {
+    $('span.cart-close').click(function () {
         remove($(this));
     });
-    function Emptycart() {
-        var unload = $('.unloaded-nav .cart-item').length;
-        var load = $('.loaded-nav .cart-item').length;
-        if ((unload <= 0) || (load <= 0)) {
-            $(".empty-cart").show();
-            $('.exist-cart').hide();
-        }
-        else {
-            return false;
-        }
-    }
 })
